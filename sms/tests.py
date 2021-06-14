e5970d5e1a123ca06c4336993119e90cdd9caeeb
will <wpulkownik@gmail.com>
2021-06-14 16:13:30 -0500

    add smstest credentials

--- sms/tests.py
+++ sms/tests.py
@@ -1,8 +1,9 @@
+from django.conf import settings
 from django.test import TestCase
 from .views import result, compose
 from .models import Contact
 from unittest import mock
-
+from twilio.rest import Client
 
 # Models Tests
 '''
@@ -20,7 +21,6 @@ class ContactTest(TestCase):
 
 '''
 TODO: - Factor SMS sending cunction in compose() into its own function sendSMS(to, from, message)
-      - put test credentials in to the .env file
       - test message sending function
       - test upload function
 
@@ -28,8 +28,23 @@ TODO: - Factor SMS sending cunction in compose() into its own function sendSMS(t
             documentation.
 '''
 
-# def test_compose_sms():
-#     message = "This is a test."
-#     to = "+18034047382"
-#     from_ = "+12017333892"
-#     assert send_message(to, from_, message) is not None
+# Test Sending SMS messages through Twilio client
+# With Test Credentials
+# messages: POST /2010-04-01/Accounts/{TestAccountSid}/Messages
+def testSms():
+    '''
+    This function tests the twilio client SMS functionality using test tokens.
+    the call returns a json response but I've only printed the message sid 
+    to just verify that things didn't break
+    read more about twilio testing here: 
+    https://www.twilio.com/docs/iam/test-credentials#test-sms-messages
+    '''
+    client = Client(settings.TEST_ACCOUNT_SID, settings.TEST_AUTHTOKEN)
+    message = client.messages.create(
+        body='The Earth is Blue, how wonderful, it is amazing.',
+        from_='+15005550006',
+        to='+18034047382'
+    )
+    
+    print(message.sid)
+    
