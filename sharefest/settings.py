ef3dc80b616eee48190470de3a7d096888494584
mongoosepk <wpulkownik@gmail.com>
2021-06-25 12:44:34 -0500

    revise documentation

--- sharefest/settings.py
+++ sharefest/settings.py
@@ -14,11 +14,8 @@ import os
 from dotenv import load_dotenv
 import django_heroku
 load_dotenv()
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -81,10 +78,6 @@ WSGI_APPLICATION = 'sharefest.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sharefestdb',
